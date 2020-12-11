09221250837d89844bb74514c662861deaf09401
svoet <31103670+svoet@users.noreply.github.com>
2020-12-11 07:20:30 +0100

    Checkmode (#2)
    * Fixed syntax issue with openssl newer than 1.0
    
    * Update ism_firmware_update.py
    
    Co-authored-by: fj1057de <74941468+fj1057de@users.noreply.github.com>

--- library/ism_firmware_update.py
+++ library/ism_firmware_update.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 #coding: UTF-8
-
 #######
 # Copyright FUJITSU LIMITED 2017-2018
 #
@@ -33,39 +32,40 @@ class IsmFirmwareUpdate():
 
     # constant
     SLEEP_SECOND = 30
-    
+
     # rest url
     FIRMWARE_LIST_REST_URL = "/ism/api/v2/system/settings/firmware/list"
     FIRMWARE_UPDATE_REST_URL = "/ism/api/v2/nodes/firmware/update"
     TASK_INFO_REST_URL = "/ism/api/v2/tasks/"
     INVENTORY_INFO_REST_URL = "/ism/api/v2/nodes/"
-    
+
     # receiving args
     module = AnsibleModule(
         argument_spec=dict(
             config=dict(required=True),
             hostname=dict(required=True),
-            firmware_update_list=dict(required=True, type="list")
-        )
+            firmware_update_list=dict(required=True, type="list"),
+        ),
+                supports_check_mode=True
     )
-    
+
     def __init__(self):
        self.__present()
-       
+
     def __present(self):
         try :
             self.module.log("argument_spec with config=" + str(self.module.params['config']) + " hostname=" + str(self.module.params['hostname']) + " firmware_update_list=" + str(self.module.params['firmware_update_list']))
-            
+
             # instance of common class
             common = IsmCommon(self.module)
-            
+
             # param check
             common.param_check(self.module.params['firmware_update_list'])
-            
+
             # convert parameters to unicode string
             self.module.params['config'] = common.covert_unicode(self.module.params['config'])
             self.module.params['hostname'] = common.covert_unicode(self.module.params['hostname'])
-            
+
             # config file open
             try :
                 config_path = path.abspath(self.module.params['config'])
@@ -73,7 +73,7 @@ class IsmFirmwareUpdate():
             except Exception as e:
                 self.module.log("file open error: " + str(config_path) + ", e=" + str(e))
                 self.module.fail_json(msg="file open error: " + str(config_path))
-            
+
             # config loads
             try :
                 json_data = json.load(f)
@@ -81,9 +81,9 @@ class IsmFirmwareUpdate():
                 f.close()
                 self.module.log("Not json format data: " + str(config_path) + ", e=" + str(e))
                 self.module.fail_json(msg="It is not json format data: " + str(config_path))
-               
+
             f.close()
-            
+
            # ip transformation
             match = re.match(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$', self.module.params['hostname'])
             if match == None:
@@ -93,20 +93,20 @@ class IsmFirmwareUpdate():
                 except Exception as e:
                     self.module.log("ip transformation error: IP address=" + str(self.module.params['hostname']) + ", e=" + str(e))
                     self.module.fail_json(msg="Host name could not be converted to IP address: " + str(self.module.params['hostname']))
-                    
+
             # ISM login
             common.ismLogin(json_data)
-            
+
             # License Check
             common.licenseCheck(usable_essential = True)
-            
+
             # get node OS
             common.getNodeOS()
-            
+
             # get node hard
             if common.getNodeId() == "":
                 common.getNodeHard()
-                
+
             # node check
             if common.getNodeId() == "":
                 self.module.log("The target host name was not found.: " + str(self.module.params['hostname']))
@@ -120,42 +120,43 @@ class IsmFirmwareUpdate():
             # firmware update execution
             firmware_update_list = []
             for update_param_hash in self.module.params['firmware_update_list']:
-                
+
                 if "firmware_version" in update_param_hash:
                     self.module.debug("***** firmwareversion specified *****")
-                    
+
                     if update_param_hash['firmware_version'] == "" or update_param_hash['firmware_version'] == None:
                         # latest firmware version
                         latest_firmware_version = self.getFirmwareVersion(common, update_param_hash['firmware_name'], update_param_hash['operation_mode'])
                         update_param_hash['firmware_version'] = latest_firmware_version
-                        
-                    # check firmware version
-                    chk = self.checkFirmwareVersion(common, update_param_hash['firmware_name'], update_param_hash['firmware_version'])
-                    
+
                 else:
                     self.module.debug("***** firmwareversion not specified *****")
-                    
+
                     # latest firmware version
                     latest_firmware_version = self.getFirmwareVersion(common, update_param_hash['firmware_name'], update_param_hash['operation_mode'])
                     update_param_hash['firmware_version'] = latest_firmware_version
-                    
-                    # check firmware version
-                    chk = self.checkFirmwareVersion(common, update_param_hash['firmware_name'], update_param_hash['firmware_version'])
-                    
-                if chk is True:
+
+                # check firmware version
+                current_version = self.getCurrentFirmwareVersion(common, update_param_hash['firmware_name'])
+                if current_version != update_param_hash['firmware_version']:
+                    update_param_hash['firmware_current_version'] = current_version
                     firmware_update_list.append(update_param_hash)
-                    
+
             # firmware update start execution
             if len(firmware_update_list) == 0:
                 result = "Success"
                 changed_result = False
             else:
-                result = self.firmwareUpdate(common, firmware_update_list)
-                changed_result = True
-                
+                if self.module.check_mode:
+                    result = dict( changed=True, original_message='', message="Firmware versions that would be changed: " + str(firmware_update_list))
+                    self.module.exit_json(**result)
+                else:
+                    result = self.firmwareUpdate(common, firmware_update_list)
+                    changed_result = True
+
             # ISM logout
             common.ismLogout()
-            
+
             # return json
             args = dict(
                 ism_firmware_update = result
@@ -169,29 +170,29 @@ class IsmFirmwareUpdate():
     def getFirmwareVersion(self, common, firmware_name, operation_mode):
         try :
             self.module.debug("***** getFirmwareVersion Start *****")
-            
+
             result_version = ""
-            
+
             # get rest url
             rest_url = common.getRestUrl(IsmFirmwareUpdate.FIRMWARE_LIST_REST_URL, "?nodeid=" + common.getNodeId())
-            
+
             # adding a response header
             add_head = " -H 'X-Ism-Authorization: " + common.getSessionId() + "' "
-            
+
             # execute rest
             exec_command = common.COMMAND + rest_url + common.HEADER + add_head + common.GET + common.HTTP_RESPONSE_CODE + common.CERTIFICATE + "'" + common.singleEscape(common.getIsmCertificate()) + "'"
             self.module.debug("exec_command = " + exec_command)
-            
+
             (rc, stdout, stderr) = common.run_command_unicode(exec_command)
             self.module.debug("rc = " + str(rc))
             self.module.debug("stdout = " + stdout)
-            
+
             if str(rc) != "0":
                 self.module.log("curl communication error: stdout=" + stdout + ". stderr=" + stderr + ". exec_command=" + exec_command)
                 self.module.fail_json(msg="curl communication error: " + stderr)
-                
+
             get_firmware_list_result = re.search(r'(.+)ISM_HTTP_RESPONSE_CODE=(\d+)', stdout.replace('\n', ''))
-            
+
             if get_firmware_list_result is None:
                 self.module.log("REST result is fault: stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                 self.module.fail_json(msg="REST result is fault: " + stderr)
@@ -205,49 +206,46 @@ class IsmFirmwareUpdate():
                 else:
                     self.module.log("Response Code=" + get_firmware_list_result.group(2) + ", stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                     self.module.fail_json(msg=get_firmware_list_result.group(1))
-            
+
             for list in json_data["IsmBody"]["FirmwareList"]:
                 if (list["FirmwareName"] == firmware_name) and (list["OperationMode"] == operation_mode):
                     result_version = list["FirmwareVersion"]
                     break
-                
+
             self.module.debug("***** getFirmwareVersion End *****")
-            
+
             return result_version
-            
+
         except Exception as e:
             self.module.log(str(e))
             self.module.fail_json(msg=str(e))
 
-    def checkFirmwareVersion(self, common, firmware_name, firmware_version):
+    def getCurrentFirmwareVersion(self, common, firmware_name):
         try :
-            self.module.debug("***** checkFirmwareVersion Start *****")
-            
+            self.module.debug("***** getCurrentFirmwareVersion Start *****")
+
             result = True
-            
-            if firmware_version == "":
-                return False
-            
+
             # get rest url
             rest_url = common.getRestUrl(IsmFirmwareUpdate.INVENTORY_INFO_REST_URL, common.getNodeId() + "/inventory/?level=All\&target=Firmware")
-            
+
             # adding a response header
             add_head = " -H 'X-Ism-Authorization: " + common.getSessionId() + "' "
-            
+
             # execute rest
             exec_command = common.COMMAND + rest_url + common.HEADER + add_head + common.GET + common.HTTP_RESPONSE_CODE + common.CERTIFICATE + "'" + common.singleEscape(common.getIsmCertificate()) + "'"
             self.module.debug("exec_command = " + exec_command)
-            
+
             (rc, stdout, stderr) = common.run_command_unicode(exec_command)
             self.module.debug("rc = " + str(rc))
             self.module.debug("stdout = " + stdout)
-            
+
             if str(rc) != "0":
                 self.module.log("curl communication error: stdout=" + stdout + ". stderr=" + stderr + ". exec_command=" + exec_command)
                 self.module.fail_json(msg="curl communication error: " + stderr)
-                
+
             check_firmware_version_result = re.search(r'(.+)ISM_HTTP_RESPONSE_CODE=(\d+)', stdout.replace('\n', ''))
-            
+
             if check_firmware_version_result is None:
                 self.module.log("REST result is fault: stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                 self.module.fail_json(msg="REST result is fault: " + stderr)
@@ -261,32 +259,31 @@ class IsmFirmwareUpdate():
                 else:
                     self.module.log("Response Code=" + check_firmware_version_result.group(2) + ", stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                     self.module.fail_json(msg=check_firmware_version_result.group(1))
-            
+
             if "Firmware" in json_data["IsmBody"]["Node"]["VariableData"]:
                 if json_data["IsmBody"]["Node"]["VariableData"]["Firmware"] is not None:
                     for list in json_data["IsmBody"]["Node"]["VariableData"]["Firmware"]:
-                        if (list["Name"] == firmware_name) and (list["FirmwareVersion"] == firmware_version):
-                            result = False
-                            break
-                            
-            self.module.debug("***** checkFirmwareVersion End *****")
-            
+                        if (list["Name"] == firmware_name):
+                            return list["FirmwareVersion"]
+
+            self.module.debug("***** getCurrentFirmwareVersion End *****")
+
             return result
-            
+
         except Exception as e:
             self.module.log(str(e))
             self.module.fail_json(msg=str(e))
-            
+
     def firmwareUpdate(self, common, firmware_update_list):
         try :
             self.module.debug("***** firmware_update Start *****")
-            
+
             # get rest url
             rest_url = common.getRestUrl(IsmFirmwareUpdate.FIRMWARE_UPDATE_REST_URL)
-            
+
             # adding a response header
             add_head = " -H 'X-Ism-Authorization: " + common.getSessionId() + "' "
-            
+
             # create rest param
             param_prefix = " '{\"IsmBody\":{\"UpdateRequest\":["
             param_suffix = "]}}' "
@@ -300,21 +297,21 @@ class IsmFirmwareUpdate():
                             "\"}"
                 param_array.append(param_tmp)
             param = param_prefix + ",".join(param_array) + param_suffix
-            
+
             # execute rest
             exec_command = common.COMMAND + rest_url + common.HEADER + add_head + common.POST + param + common.HTTP_RESPONSE_CODE + common.CERTIFICATE + "'" + common.singleEscape(common.getIsmCertificate()) + "'"
             self.module.debug("exec_command = " + exec_command)
-            
+
             (rc, stdout, stderr) = common.run_command_unicode(exec_command)
             self.module.debug("rc = " + str(rc))
             self.module.debug("stdout = " + stdout)
-            
+
             if str(rc) != "0":
                 self.module.log("curl communication error: stdout=" + stdout + ". stderr=" + stderr + ". exec_command=" + exec_command)
                 self.module.fail_json(msg="curl communication error: " + stderr)
-            
+
             firmware_update_result = re.search(r'(.+)ISM_HTTP_RESPONSE_CODE=(\d+)', stdout.replace('\n', ''))
-            
+
             if firmware_update_result is None:
                 self.module.log("REST result is fault: stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                 self.module.fail_json(msg="REST result is fault: " + stderr)
@@ -329,55 +326,55 @@ class IsmFirmwareUpdate():
                 else:
                     self.module.log("Response Code=" + firmware_update_result.group(2) + ", stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                     self.module.fail_json(msg=firmware_update_result.group(1))
-            
+
             # task check
             task_result = self.taskCheck(task_id, common)
-            
+
             self.module.log("firmwareUpdate success")
             self.module.debug("***** firmware_update End *****")
-            
+
             return task_result
-            
+
         except Exception as e:
             self.module.log(str(e))
             self.module.fail_json(msg=str(e))
-            
+
     def taskCheck(self, id, common):
         try :
             self.module.debug("***** taskCheck Start *****")
-            
+
             now = datetime.datetime.now()
             unix_time = long(time.mktime(now.timetuple()))
             time_out = long(unix_time) + long(IsmUserSettingsValue.FIRMWARE_UPDATE_TIME_OUT)
-            
+
             # get rest url
             rest_url = common.getRestUrl(IsmFirmwareUpdate.TASK_INFO_REST_URL, id)
-            
+
             # adding a response header
             add_head = " -H 'X-Ism-Authorization: " + common.getSessionId() + "' "
-            
+
             # waiting for task
             self.module.debug("***** waiting for task Start *****")
             while True:
-            
+
                 now = datetime.datetime.now()
                 now_time = long(time.mktime(now.timetuple()))
-                
+
                 if long(now_time) < long(time_out):
                     # execute rest
                     exec_command = common.COMMAND + rest_url + common.HEADER + add_head + common.HTTP_RESPONSE_CODE + common.CERTIFICATE + "'" + common.singleEscape(common.getIsmCertificate()) + "'"
                     self.module.debug("exec_command = " + exec_command)
-                    
+
                     (rc, stdout, stderr) = common.run_command_unicode(exec_command)
                     self.module.debug("rc = " + str(rc))
                     self.module.debug("stdout = " + stdout)
-                    
+
                     if str(rc) != "0":
                         self.module.log("curl communication error: stdout=" + stdout + ". stderr=" + stderr + ". exec_command=" + exec_command)
                         self.module.fail_json(msg="curl communication error: " + stderr)
-                    
+
                     task_check_result = re.search(r'(.+)ISM_HTTP_RESPONSE_CODE=(\d+)', stdout.replace('\n', ''))
-                    
+
                     if task_check_result is None:
                         self.module.log("REST result is fault: stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                         self.module.fail_json(msg="REST result is fault: " + stderr)
@@ -393,7 +390,7 @@ class IsmFirmwareUpdate():
                             self.module.log("Response Code=" + task_check_result.group(2) + ", stdout=" + stdout + ", stderr=" + stderr + ", exec_command=" + exec_command)
                             self.module.fail_json(msg=task_check_result.group(1))
                             break
-                        
+
                     # end loop if status is complete
                     if status == "Complete" or status == "Cancel-Complete":
                         result = json_data["IsmBody"]["Result"]
@@ -403,12 +400,12 @@ class IsmFirmwareUpdate():
                     self.module.log("waiting for task timeout: " + str(IsmUserSettingsValue.FIRMWARE_UPDATE_TIME_OUT) + "s")
                     self.module.fail_json(msg="waiting for task timeout: " + str(IsmUserSettingsValue.FIRMWARE_UPDATE_TIME_OUT) + "s")
                     break
-                    
+
                 # execute sleep
                 time.sleep(IsmFirmwareUpdate.SLEEP_SECOND)
-                
+
             self.module.debug("***** taskCheck End *****")
-            
+
             if result == "Error":
                 errmsg = "[TaskId]=" + id + " "
                 for SubTaskInfoList in json_data["IsmBody"]["SubTaskInfoList"]:
@@ -426,9 +423,9 @@ class IsmFirmwareUpdate():
             else:
                 self.module.debug("task result:" + result)
                 return result
-                
+
         except Exception as e:
             self.module.log(str(e))
             self.module.fail_json(msg=str(e))
-            
+
 IsmFirmwareUpdate()
